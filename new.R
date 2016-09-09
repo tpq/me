d443cad5c652daf1b50c9fa5f668aa79b9a29957
Thom Quinn <contacttomquinn@gmail.com>
2016-09-09 13:31:10 +1000

    Move 'insert' function to miSciTools package

--- new.R
+++ new.R
@@ -1,26 +1,3 @@
-insert <- function(file, after, what){
-  
-  # Read lines from connection
-  con <- file(file)
-  lines <- readLines(con)
-  
-  # Find where to insert the new line
-  i <- which(grepl(after, lines))[1]
-  
-  # Insert the new line
-  if(i == length(lines)){
-    
-    new <- c(lines[1:i], as.character(what))
-    
-  }else{
-    new <- c(lines[1:i], as.character(what), lines[(i+1):length(lines)])
-  }
-  
-  # Write lines to connection
-  writeLines(new, con = con)
-  close(con)
-}
-
 newMusings <- function(blog.title = "New Title", after = "### 2016"){
   
   # Write custom blog template to new file
@@ -48,19 +25,19 @@ Placeholder
   
   # Add blog index to musings.html
   blog.link <- paste0("##### [**", blog.title, "**](", blog.name, ".html)")
-  insert("musings.Rmd", after = after, what = blog.link)
-  insert("musings.Rmd", after = after, what = "")
+  miSciTools::insert("musings.Rmd", after = after, what = blog.link)
+  miSciTools::insert("musings.Rmd", after = after, what = "")
   
   # Add blog index to feed.rss
   rss.loc <- "<description>Personal website of Thom Quinn</description>"
-  insert("feed.rss", after = rss.loc, what = "  </index>")
-  insert("feed.rss", after = rss.loc, what = "    <description>[PLACEHOLDER]</description>")
+  miSciTools::insert("feed.rss", after = rss.loc, what = "  </index>")
+  miSciTools::insert("feed.rss", after = rss.loc, what = "    <description>[PLACEHOLDER]</description>")
   warning("Make sure to edit the RSS feed (feed.rss) with a proper description!")
   rss.link <- paste0("    <link>http://www.http://tpq.me/", blog.name, ".html</link>")
-  insert("feed.rss", after = rss.loc, what = rss.link)
+  miSciTools::insert("feed.rss", after = rss.loc, what = rss.link)
   rss.title <- paste0("    <title>", blog.title, "</title>")
-  insert("feed.rss", after = rss.loc, what = rss.title)
-  insert("feed.rss", after = rss.loc, what = "  <index>")
+  miSciTools::insert("feed.rss", after = rss.loc, what = rss.title)
+  miSciTools::insert("feed.rss", after = rss.loc, what = "  <index>")
 }
 
 newFiction <- function(blog.title = "New Title", after = "### Poetry"){
@@ -86,6 +63,6 @@ Placeholder
   
   # Add blog index to musings.html
   blog.link <- paste0("##### [**", blog.title, "**](", blog.name, ".html)")
-  insert("fiction.Rmd", after = after, what = blog.link)
-  insert("fiction.Rmd", after = after, what = "")
+  miSciTools::insert("fiction.Rmd", after = after, what = blog.link)
+  miSciTools::insert("fiction.Rmd", after = after, what = "")
 }
